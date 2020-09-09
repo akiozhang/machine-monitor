2263a32a8f3bea86203b71d35904420a25a5b8df
zhangwei <260278>
2020-09-09 14:52:42 +0800

    update

---
+++ src/components/table/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <el-table
+    :data="dataList"
+    border
+    v-loading="dataListLoading"
+    @selection-change="selectionChangeHandle"
+    style="width: 100%;">
+<!--    <el-table-column-->
+<!--      type="selection"-->
+<!--      header-align="center"-->
+<!--      align="center"-->
+<!--      width="50">-->
+<!--    </el-table-column>-->
+    <el-table-column
+      prop="number"
+      header-align="center"
+      align="center"
+      width="80"
+      label="相机编号">
+    </el-table-column>
+    <el-table-column
+      prop="remark"
+      header-align="center"
+      align="center"
+      label="相机IP">
+    </el-table-column>
+    <el-table-column
+      prop="jobId"
+      header-align="center"
+      align="center"
+      width="80"
+      label="事件类型">
+    </el-table-column>
+    <el-table-column
+      prop="beanName"
+      header-align="center"
+      align="center"
+      label="事件描述">
+    </el-table-column>
+    <el-table-column
+      prop="params"
+      header-align="center"
+      align="center"
+      label="危险等级">
+    </el-table-column>
+    <el-table-column
+      prop="status"
+      header-align="center"
+      align="center"
+      label="状态">
+      <template slot-scope="scope">
+        <el-tag v-if="scope.row.status === 0" size="small">正常</el-tag>
+        <el-tag v-else size="small" type="danger">暂停</el-tag>
+      </template>
+    </el-table-column>
+<!--    <el-table-column-->
+<!--      fixed="right"-->
+<!--      header-align="center"-->
+<!--      align="center"-->
+<!--      width="150"-->
+<!--      label="操作">-->
+<!--      <template slot-scope="scope">-->
+<!--        <el-button v-if="isAuth('sys:schedule:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.jobId)">修改</el-button>-->
+<!--        <el-button v-if="isAuth('sys:schedule:delete')" type="text" size="small" @click="deleteHandle(scope.row.jobId)">删除</el-button>-->
+<!--        <el-button v-if="isAuth('sys:schedule:pause')" type="text" size="small" @click="pauseHandle(scope.row.jobId)">暂停</el-button>-->
+<!--        <el-button v-if="isAuth('sys:schedule:resume')" type="text" size="small" @click="resumeHandle(scope.row.jobId)">恢复</el-button>-->
+<!--        <el-button v-if="isAuth('sys:schedule:run')" type="text" size="small" @click="runHandle(scope.row.jobId)">立即执行</el-button>-->
+<!--      </template>-->
+<!--    </el-table-column>-->
+  </el-table>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {
+        dataForm: {
+          beanName: ''
+        },
+        dataList: [],
+        pageIndex: 1,
+        pageSize: 10,
+        totalPage: 0,
+        dataListLoading: false,
+        dataListSelections: [],
+        addOrUpdateVisible: false,
+        logVisible: false
+      }
+    },
+    methods: {
+      selectionChangeHandle (val) {
+        this.dataListSelections = val
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -1,39 +1,98 @@
 <template>
   <div class="mod-home">
-    <h3>项目介绍</h3>
-    <ul>
-      <li>renren-fast-vue基于vue、element-ui构建开发，实现<a href="https://gitee.com/renrenio/renren-fast" target="_blank">renren-fast</a>后台管理前端功能，提供一套更优的前端解决方案</li>
-      <li>前后端分离，通过token进行数据交互，可独立部署</li>
-      <li>主题定制，通过scss变量统一一站式定制</li>
-      <li>动态菜单，通过菜单管理统一管理访问路由</li>
-      <li>数据切换，通过mock配置对接口数据／mock模拟数据进行切换</li>
-      <li>发布时，可动态配置CDN静态资源／切换新旧版本</li>
-      <li>演示地址：<a href="http://demo.open.renren.io/renren-fast" target="_blank">http://demo.open.renren.io/renren-fast</a> (账号密码：admin/admin)</li>
-    </ul>
-    <h3>获取帮助</h3>
-    <ul>
-      <li>官方社区：<a href="https://www.renren.io/community" target="_blank">https://www.renren.io/community</a></li>
-      <li>前端Git地址：<a href="https://github.com/renrenio/renren-fast-vue" target="_blank">https://github.com/renrenio/renren-fast-vue</a></li>
-      <li>后台Git地址：<a href="https://gitee.com/renrenio/renren-fast" target="_blank">https://gitee.com/renrenio/renren-fast</a></li>
-      <li>代码生成器：<a href="https://gitee.com/renrenio/renren-generator" target="_blank">https://gitee.com/renrenio/renren-generator</a></li>
-      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
-    </ul>
-    <h3>官方QQ群</h3>
-    <ul>
-      <li>高级群：324780204(大牛云集，跟大牛学习新技能)</li>
-      <li>普通群：145799952(学习交流，互相解答各种疑问)</li>
-    </ul>
+    <div class="mod-container">
+      <div class="mod-home__select">
+        <el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </div>
+      <div class="mod-home__video">
+        <video src="static/video/test.mp4" controls="controls"></video>
+      </div>
+    </div>
+    <div class="mod-container__title">
+      <span>报警信息</span>
+    </div>
+    <div class="mod-home__table">
+      <b-table></b-table>
+    </div>
   </div>
 </template>
 
 <script>
+  import bTable from '@/components/table'
   export default {
+    name: 'home',
+    components: {
+      bTable
+    },
+    data () {
+      return {
+        options: [{
+          value: '001',
+          label: '设备1'
+        }, {
+          value: '002',
+          label: '设备2'
+        }, {
+          value: '003',
+          label: '设备3'
+        }, {
+          value: '004',
+          label: '设备4'
+        }, {
+          value: '005',
+          label: '设备5'
+        }],
+        value: '001',
+        centerDialogVisible: false
+      }
+    },
+    computed: {
+      getMachineTitle () {
+        let _this = this
+        let result = ''
+        _this.options.forEach((item) => { if (item['value'] === _this.value) result = item['label'] })
+        return result
+      }
+    },
+    methods: {
+      onClickMachine () {
+        console.log(this.getMachineTitle)
+        this.centerDialogVisible = true
+      }
+    }
   }
 </script>
 
-<style>
+<style lang="scss" scoped>
   .mod-home {
     line-height: 1.5;
+    &__select{
+      width: 20%;
+      min-width: 200px;
+    }
+    &__video{
+      text-align: center;
+      video{
+        max-height: 300px;
+        width: 80%;
+      }
+    }
+  }
+  .mod-container{
+    min-height: 100px;
+    line-height: 100px;
+    &__title{
+      font-size: 20px;
+      padding: 20px;
+      font-weight: bold;
+   }
   }
 </style>
 
